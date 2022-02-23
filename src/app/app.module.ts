c0609cf2b73797f730042284a50c9f5ff5df2924
Darken513 <affesachraf70@gmail.com>
2022-02-23 15:18:19 +0100

    init final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {InputTextModule} from 'primeng-lts/inputtext';
 
+import { AppointementsModule } from './modules/appointements/appointements.module';
+import { ArticlesModule } from './modules/articles/articles.module';
+import { ClientsModule } from './modules/clients/clients.module';
+import { MedicinesModule } from './modules/medicines/medicines.module';
+import { PharmaciesModule } from './modules/pharmacies/pharmacies.module';
+import { QuestionsModule } from './modules/questions/questions.module';
+import { SharedModule } from './modules/shared/shared.module';
+
+import {InputTextModule} from 'primeng-lts/inputtext';
 @NgModule({
   declarations: [
     AppComponent
@@ -12,6 +22,17 @@ import {InputTextModule} from 'primeng-lts/inputtext';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    
+    AppointementsModule,
+    ArticlesModule,
+    ClientsModule,
+    MedicinesModule,
+    PharmaciesModule,
+    QuestionsModule,
+    SharedModule,
+
     InputTextModule
   ],
   providers: [],
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
-
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
   ],
   exports:[
-    //remember to export your final main components
+    
   ]
 })
 export class SharedModule { }
