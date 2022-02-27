5b67101352229963ff95162da0efd700058d2e62
Saifallah El Mouelhi <saifallah.elmouelhi@etudiant-fst.utm.tn>
2022-02-27 23:55:50 +0100

    commit search question2

--- src/app/modules/clients/sign-up-doctor.service.ts
+++ src/app/modules/clients/sign-up-doctor.service.ts
@@ -7,7 +7,7 @@ import { Doctor } from '../shared/models/Doctor';
   providedIn: 'root'
 })
 export class SignUpDoctorService {
-  apiURL: string = "http://localhost:8082/"
+  apiURL: string = "http://localhost:8080/"
 
   constructor(private http: HttpClient) { }
     signUp(doctor: Doctor): Observable<void> {
